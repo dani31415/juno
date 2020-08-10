eedd3e9af9964f7e91d5b1c7d3a7d56ffbc5175c
Dani Marques Sole <dani@wiris.com>
2020-08-10 17:19:49 +0200

    Fixed and added suport to @angular/material

--- apps/front/src/app/app.module.ts
+++ apps/front/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { AppRouterModule } from './app-router.module';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRouterModule
   ],
   providers: [],
--- apps/front/src/app/task/task.module.ts
+++ apps/front/src/app/task/task.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from "@angular/forms"; 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { TaskListComponent } from './list/task-list.component';
 import { TaskService } from './task.service';
 import { TaskRouterModule } from './task-router.module';
 import { TaskEditComponent } from './edit/task-edit.component';
+
 
 @NgModule({
   providers: [
@@ -14,6 +17,8 @@ import { TaskEditComponent } from './edit/task-edit.component';
   imports: [
     CommonModule,
     FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
     TaskRouterModule
   ],
   declarations: [
